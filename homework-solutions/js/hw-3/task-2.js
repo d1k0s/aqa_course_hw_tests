910c15911f32503b33f79b5e4fe026ee75b22b3c
d1k0s <dkossics@gmail.com>
2025-07-31 16:45:19 +0300

    hw-3 task-2

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,3 +4,10 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+
+const n = "7";
+const nn = parseInt(n + n);
+const nnn = parseInt(n + n + n);
+
+console.log(parseInt(n) + nn + nnn);
+
