aafc3e260119837125079dd601218265cca99142
d1k0s <dkossics@gmail.com>
2025-09-09 18:06:10 +0300

    hw-12

---
+++ homework-solutions/js/hw-12/task-1.mjs
@@ -0,0 +1,56 @@
+import { ConfigSet } from "ts-jest";
+
+function delayTwoSeconds(delay) {
+    setTimeout(delay, 2000)
+} 
+
+const promise = new Promise((resolve) => resolve(1));
+promise.then((result) => console.log(result));
+
+const newPromise = new Promise((resolve, reject) => reject("Error"))
+newPromise.then((result) => console.log(result))
+            .catch((error) => console.log(error));
+
+function prmiseNumber(n) {
+    return new Promise((resolve, reject) => resolve(n))
+}
+console.log(prmiseNumber(5))
+
+Promise.all([prmiseNumber(1), prmiseNumber(2), prmiseNumber(3)])
+    .then((values) => console.log(values))
+    .catch((error) => console.log("Error"))
+
+Promise.allSettled([prmiseNumber(1), prmiseNumber(2), prmiseNumber(3)]).then((values) => {
+    values.forEach((value) => {
+        if (value.status === "fulfilled") {
+            console.log('Fulfilled:', value.value)
+        } else {
+            console.log('Rejected:', value.reason)
+        }
+    })
+})
+
+async function handleAllPromises() {
+    try {
+        const values = await Promise.all([prmiseNumber(1), prmiseNumber(2), prmiseNumber(3)])
+        console.log()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function handleAllSettledPromises() {
+    try {
+        const results = await Promise.allSettled([prmiseNumber(1), prmiseNumber(2), prmiseNumber(3)]);
+
+        results.forEach((result) => {
+            if (result.status === "fulfilled") {
+                console.log(`Fulfilled: ${result.value}`);
+            } else {
+                console.log(`Rejected: ${result.reason}`);
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
